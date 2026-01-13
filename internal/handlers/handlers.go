56c5d21ef5f16c91cd8db7c05c68697286d8e6a6
codercollo <itscollinsmaina@gmail.com>
2026-01-13 23:15:18 +0300

    Improved WS endpoint, setting goroutines

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,12 +1,17 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gorilla/websocket"
 )
+
+var wsChan = make(chan WsPayload)
+
+var clients = make(map[WebSocketConnection]string)
 
 // Initialize Jet template set
 var views = jet.NewSet(
@@ -29,7 +34,11 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 
 	}
+}
 
+// WebSocketConnection wraps a websocket.Conn for easier methos access
+type WebSocketConnection struct {
+	*websocket.Conn
 }
 
 // WsJsonResponse defines the structure of JSON messages sent over WebSocket
@@ -37,6 +46,14 @@ type WsJsonResponse struct {
 	Action      string `json:"action"`
 	Message     string `json:"message"`
 	MessageType string `json:"message_type"`
+}
+
+// WsPayload represents a WebSocket message with action, sender, content and connection
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
 }
 
 // WsEndPoint handles WebSocket connections
@@ -54,10 +71,60 @@ func WsEndPoint(w http.ResponseWriter, r *http.Request) {
 	var response WsJsonResponse
 	response.Message = `<em><small>Connected to server </small><em>`
 
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
+
 	//Send initial message to client
 	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
+	}
+
+	go ListenForWs(&conn)
+
+}
+
+func ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			//
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	var response WsJsonResponse
+
+	for {
+		e := <-wsChan
+
+		response.Action = "Got here"
+		response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
+		broadcastToAll(response)
+
+	}
+}
+
+func broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket err")
+			_ = client.Close()
+			delete(clients, client)
+		}
 	}
 
 }
